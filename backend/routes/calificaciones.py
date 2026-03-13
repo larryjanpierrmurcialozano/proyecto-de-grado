1e9a38ced244f17cc9b774d33ca4dcede4cdfc5b
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 19:57:10 -0500

    autosave: 2026-03-12T19:57:09 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -4,6 +4,6 @@
 # todo el modulo de calificaciones funciona como un sistema local, el cual usara los archivos, tanto a nivel locar como a nivel de servidor, 
 # para almacenar las calificaciones, esto con el fin de evitar problemas de concurrencia y bloqueo de base de datos, 
 # ademas de que se podra usar en modo offline, y luego sincronizar con el servidor cuando se tenga conexion a internet.
-#    
+# todo    
 from typing import List
 import mysql.connector
