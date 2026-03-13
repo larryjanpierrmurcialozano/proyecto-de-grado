871f41c6a770cec50d9e48560d8c3da87d839303
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 19:55:03 -0500

    autosave: 2026-03-12T19:55:03 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -1,6 +1,6 @@
 # Calificaciones    
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-frpm typing import List
+from typing import List
 from ..database import get_db
 import mysql.connector
