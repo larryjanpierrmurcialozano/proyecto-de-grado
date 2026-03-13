387a942d2ea2c9bc195c42b7f913c5888974d746
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 19:57:40 -0500

    autosave: 2026-03-12T19:57:40 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -4,6 +4,6 @@
 # todo el modulo de calificaciones funciona como un sistema local, el cual usara los archivos, tanto a nivel locar como a nivel de servidor, 
 # para almacenar las calificaciones, esto con el fin de evitar problemas de concurrencia y bloqueo de base de datos, 
 # ademas de que se podra usar en modo offline, y luego sincronizar con el servidor cuando se tenga conexion a internet.
-# todo se tratara de guardar en formato excel, usando libreria openpyxl   
+# todo se tratara de guardar en formato excel, usando libreria openpyxl, y se guardara en una carpeta especifica para cada curso, dentro de una carpeta general de calificaciones, esto con el fin de mantener un orden y facilitar la busqueda de las calificaciones.   
 from typing import List
 import mysql.connector
