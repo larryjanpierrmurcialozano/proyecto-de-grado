ac002277dc77b487b4771fbb54c4faee2b3661ca
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 20:13:29 -0500

    autosave: 2026-03-12T20:13:29 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -8,7 +8,7 @@
 # dentro de una carpeta general de calificaciones, esto con el fin de mantener un orden y facilitar la busqueda de las calificaciones. 
 #   todo se tendra una estructura de carpetas asi:
 #   calificaciones/
-#       grado 6/ 
+#       grado 6#/ 
 #                grupo #/
 #           calificaciones.xlsx
 #       grado 7/
