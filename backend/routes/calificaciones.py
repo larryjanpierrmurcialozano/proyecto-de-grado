4cea187aebb40d4fc8ac03fefb63d35f80186b1b
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 19:55:35 -0500

    autosave: 2026-03-12T19:55:35 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -1,3 +1,3 @@
-# Calificaciones sistema de gestion de calificaciones,     
+# Calificaciones sistema de gestion de calificaciones, CRUD de calificaciones, listado de calificaciones por curso, etc.    
 from typing import List
 import mysql.connector
