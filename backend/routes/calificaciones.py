4af8d6a8aa620f1fc3271c9aaa5e3375beab0c8f
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 21:12:53 -0500

    autosave: 2026-03-12T21:12:52 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -54,13 +54,4 @@ from flask import Blueprint, jsonify, request, session
 from utils.database import get_db
 from utils.helpers import _error_interno
 calificaciones_bp = Blueprint('calificaciones', __name__)
-@calificaciones_bp.route('/calificaciones', methods=['GET'])
-def get_calificaciones():
-    try:
-        db = get_db()
-        cursor = db.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM calificaciones")
-        calificaciones = cursor.fetchall()
-        return jsonify(calificaciones)
-    except mysql.connector.Error as err:
-        return _error_interno(str(err))
+def api
