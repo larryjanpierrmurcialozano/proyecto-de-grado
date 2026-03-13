fa313a2087f0759b07d5a0f50a206644436ec324
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 20:03:35 -0500

    autosave: 2026-03-12T20:03:35 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -32,7 +32,7 @@
 # sin embargo la idea es que se autogeneren estas funcionalidad de manera dinamica, es decir, 
 # que se tenga una ruta general para cada curso, y que se maneje el archivo excel de manera dinamica, 
 # sin necesidad de tener una ruta especifica para cada curso, esto con el fin de evitar la redundancia y facilitar el mantenimiento del codigo.
-# tambien hay que tener claro que lo que se busca es que al generar el excel de cada grado se autogenere con el valor seleccionado salido de la lista, tipo en caso de seleccionar el grado 7, que se genera
+# tambien hay que tener claro que lo que se busca es que al generar el excel de cada grado se autogenere con el valor seleccionado salido de la lista, tipo en caso de seleccionar el grado 7, que se genere el excel 
 from typing import List
 import mysql.connector
 from flask import Blueprint, jsonify, request, session
