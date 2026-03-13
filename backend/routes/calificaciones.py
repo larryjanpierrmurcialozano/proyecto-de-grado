6a58c85ed659aa811200ac331e4c1ac91c7d8310
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 19:56:24 -0500

    autosave: 2026-03-12T19:56:24 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -1,6 +1,6 @@
 # Calificaciones sistema de gestion de calificaciones, CRUD de calificaciones, listado de calificaciones por curso, etc. 
 # ═══════════════════════════════════════════════════════════════════════════════
 # BLUEPRINT: CALIFICACIONES — CRUD de calificaciones, listado de calificaciones por curso
-# todo el modulo de calificaciones funciona como un sistema local, el cual usara los archivos locales como referencia    
+# todo el modulo de calificaciones funciona como un sistema local, el cual usara los archivos locales    
 from typing import List
 import mysql.connector
