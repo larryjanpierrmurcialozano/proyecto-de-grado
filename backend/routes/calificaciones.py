1e6df969d29f55897ac761be6abb48ec1d1cddf3
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-12 20:01:14 -0500

    autosave: 2026-03-12T20:01:14 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -22,7 +22,7 @@
 #           calificaciones.xlsx / todo para cada curso
 #  el chiste de todo es usar archivos excel como referencia, que sean editable dentro de la aplicacion 
 # y que se puedan descargar o subir al sistema para asi mantener cierta flexibilidad y facilidad de uso.
-
+#
 from typing import List
 import mysql.connector
 from flask import Blueprint, jsonify, request, session
