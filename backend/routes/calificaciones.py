9398638a0f95e968fcc5ae5076452a6192fbc99b
larryjanpierrmurcialozano <larry_murcialo@fet.edu.co>
2026-03-13 22:09:31 -0500

    autosave: 2026-03-13T22:09:31 - backend\routes\calificaciones.py

--- backend/routes/calificaciones.py
+++ backend/routes/calificaciones.py
@@ -6,7 +6,7 @@
 # Calificaciones sistema de gestion de calificaciones, CRUD de calificaciones, listado de calificaciones por curso, etc. 
 # ═══════════════════════════════════════════════════════════════════════════════
 # BLUEPRINT: CALIFICACIONES — CRUD de calificaciones, listado de calificaciones por curso
-# todo el modulo de calificaciones funciona como un sistema local, el cual usara los archivos, tanto a nivel locar como a nivel de servidor, 
+# todo el modulo de calificaciones funciona como un sistema local, el cual usara los archivos, tanto a nivel local como a nivel de servidor, 
 # para almacenar las calificaciones, esto con el fin de evitar problemas de concurrencia y bloqueo de base de datos, 
 # ademas de que se podra usar en modo offline, y luego sincronizar con el servidor cuando se tenga conexion a internet.
 # todo se tratara de guardar en formato excel, usando libreria openpyxl, y se guardara en una carpeta especifica para cada curso, 
